a1816c7ef76c9584e3270f8113a406909f228d85
SergioS0ares <sergyn200295@gmail.com>
2025-03-29 15:02:04 -0300

    implementa demonstração completa de bindings e template variables
    - Adiciona o componente TemplateBindingComponent com exemplos práticos de:
      • Text Interpolation
      • Property Binding
      • Attribute Binding
      • Class e Style Binding (condicional e dinâmico)
      • Event Binding com click, keydown e mousemove
      • Two-way Binding com [(ngModel)]
      • ngClass e ngStyle
    
    - Adiciona o componente TemplateVariablesComponent com:
      • Uso de variáveis de template (#var)
      • Integração com ViewChild e acesso ao DOM via ElementRef
      • Acesso ao conteúdo de um componente filho via ViewChild
    
    - Ajustes visuais e lógicos para melhor entendimento do comportamento reativo nos templates Angular.

--- meu-primeiro-projeto-latest/src/app/app.component.ts
+++ meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -1,21 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NewComponent } from './components/new-component/new-component.component';
-import { TemplateBindingComponent } from './components/template-binding/template-binding.component';
+import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
+import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NewComponent, TemplateBindingComponent],
+  imports: [RouterOutlet, NewComponent, TemplateBindingComponent, TemplateVariablesComponent],
   template:`<router-outlet></router-outlet>
 <h1>APRENDIZADO ANGULAR</h1>
 <div class="theme-red">
   <app-new-component />
 </div>
 <app-template-binding />
+
+<app-template-variables />
 `,
 })
 export class AppComponent {
   title = 'meu-primeiro-projeto-latest';
+  
 }
 
 
--- meu-primeiro-projeto-latest/src/app/components/template/template-binding/template-binding.component.ts
+++ meu-primeiro-projeto-latest/src/app/components/template/template-binding/template-binding.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-template-binding',
-  imports: [],
+  imports: [FormsModule, CommonModule, NgClass,],
   templateUrl: './template-binding.component.html',
   styleUrl: './template-binding.component.scss'
 })
@@ -11,6 +13,8 @@ public name: string = 'Sergio';
 public age: number = 21;
 public isDisabled = true;
 public scrValue = 'https://th.bing.com/th/id/OIP.eBV6NKOwkJSZXDufD45I3wHaE8?rs=1&pid=ImgDetMain'
+
+public condition = this.age > 1 ? 'Teste' : 'Teste2';
 
 public sum(val1:number, val2: number){
   return val1 + val2;
@@ -21,5 +25,26 @@ constructor() {
     this.name = 'João e Maria';
   }, 1000);
 }
+
+public isTextDecoration = this.age >= 32 ? 'underline' : 'none';
+
+public sumAge(){
+  return this.age++;
+}
+public subAge(){
+  return this.age--;
+}
+
+public onKeyDown(event: Event){
+  return console.log(event);
+}
+
+public onMouseMove(event: any){
+  return console.log({
+    clietX: event.clietX,
+    clietY: event.clietY,
+  });
+}
+
 
 }
